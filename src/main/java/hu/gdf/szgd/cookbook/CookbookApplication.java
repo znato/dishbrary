31d9b1e3363f35bad1cae5c7bc1ff4ed5c50c0e2
znato <zoltan.nato.sp@lhsystems.com>
2018-12-15 14:08:04 +0100

    cookbook initial project commit

---
+++ src/main/java/hu/gdf/szgd/cookbook/CookbookApplication.java
@@ -0,0 +1,14 @@
+package hu.gdf.szgd.cookbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookbookApplication.class, args);
+	}
+
+}
+
